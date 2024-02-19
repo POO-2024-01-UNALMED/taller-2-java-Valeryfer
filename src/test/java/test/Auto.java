05b85305af70f90c491a4bb4d52c5c631a0ed6a4
Valeryfer <159395606+Valeryfer@users.noreply.github.com>
2024-02-18 19:54:47 -0500

    Métodos corregidos

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -3,13 +3,13 @@ package test;
 public class Auto {
 	String modelo;
 	int precio;
-	Asiento[] asientos;
+	Asiento [] asientos;
 	String marca;
 	Motor motor;
 	int registro;
 	static int cantidadCreados;
 	
-	public int cantidadAsientos() {
+	int cantidadAsientos() {
 		int numeroAsientos = 0;
 		for (int i = 0 ; 1 < asientos.lenght; i++) {
 			if (asientos[i] != null) {
@@ -19,7 +19,7 @@ public class Auto {
 		return numeroAsientos;
 	}
 
-	public String verificarIntegridad() {
+	String verificarIntegridad() {
 		if (this.registro) == (this.motor.registro) {
 			for (int i = 0; i < asientos.lenght; i++) {
 				if (asientos[i] !=  null) {
